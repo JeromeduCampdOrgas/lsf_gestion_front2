4cfcf205da7d29f194480459b000c4157658b96a
Jerome <ducampdorgasjerome@gmail.com>
2022-06-28 17:16:39 +0200

    modification userAdminForm

--- src/components/admin/utilisateurs/UserAdminForm.jsx
+++ src/components/admin/utilisateurs/UserAdminForm.jsx
@@ -8,6 +8,10 @@ import {
 import { deleteSelection } from "../../../feature/users/selectedUserSlice";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { useState } from "react";
+
+/*********  CSS **************/
+import "../../../styles/Admin/users/userAdminForm.scss";
 
 const UserAdminForm = () => {
   let navigate = useNavigate();
@@ -15,12 +19,13 @@ const UserAdminForm = () => {
 
   const usersList = useSelector((state) => state.usersList[0]);
   const selectedUserId = useSelector((state) => state.selectedUser);
-  const userDataLength = useSelector((state) => state.selectedUserData);
-
+  const selectedUserData = useSelector((state) => state.selectedUserData);
+  const listRoles = useSelector((state) => state.roles);
   useEffect(() => {
     const userData = usersList.filter(
       (user) => user.id === parseInt(selectedUserId)
     );
+
     dispatch(addUserData(userData));
   }, []);
 
@@ -40,23 +45,106 @@ const UserAdminForm = () => {
   };
   return (
     <Provider store={store}>
-      <div>
+      <div id="user-admin-form">
         <h1>formulaire de modification</h1>
-        <input id="nom" type="text" placeholder="votre nom" />
-        <input id="prenom" type="text" placeholder="votre prénom" />
-        <input id="email" type="text" placeholder="votre email" />
-        <input id="password" type="password" placeholder="mot de passe" />
-        <div>
-          <input id="btn-valid" className="btn" type="button" value="Valider" />
 
-          <input
-            id="btn-escape"
-            className="btn"
-            type="button"
-            value="Annuler"
-            onClick={() => retour()}
-          />
-        </div>
+        {selectedUserData[0] ? (
+          <form>
+            <label className="user-info" htmlFor="nom">
+              Nom:
+              <input
+                id="nom"
+                type="text"
+                placeholder={selectedUserData[0].nom}
+              />
+            </label>
+            <label className="user-info" htmlFor="prenom">
+              Prénom:
+              <input
+                id="prenom"
+                type="text"
+                placeholder={selectedUserData[0].prenom}
+              />
+            </label>
+            <label className="user-info" htmlFor="email">
+              Email:
+              <input
+                id="email"
+                type="text"
+                placeholder={selectedUserData[0].email}
+              />
+            </label>
+            <label className="user-info" htmlFor="tel">
+              Tel:
+              <input
+                id="tel"
+                type="text"
+                placeholder={selectedUserData[0].tel}
+              />
+            </label>
+            <label className="user-info" htmlFor="n_rue">
+              N°de rue:
+              <input
+                id="n_rue"
+                type="text"
+                placeholder={selectedUserData[0].n_rue}
+              />
+            </label>
+            <label className="user-info" htmlFor="rue">
+              Rue:
+              <input
+                id="rue"
+                type="text"
+                placeholder={selectedUserData[0].rue}
+              />
+            </label>
+            <label className="user-info" htmlFor="cp">
+              Code postal:
+              <input id="cp" type="text" placeholder={selectedUserData[0].cp} />
+            </label>
+            <label className="user-info" htmlFor="ville">
+              Commune:
+              <input
+                id="ville"
+                type="text"
+                placeholder={selectedUserData[0].ville}
+              />
+            </label>
+            <label className="user-info" htmlFor="roles">
+              Rôle:
+              <select
+                name="roles"
+                id="roles"
+                value={selectedUserData[0].role}
+                onChange={(event) => console.log(event.target.value)}
+              >
+                {listRoles[0].map((role) => (
+                  <option key={role} value={role}>
+                    {role}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <div id="buttons">
+              <input
+                id="btn-valid"
+                className="btn"
+                type="button"
+                value="Valider"
+              />
+
+              <input
+                id="btn-escape"
+                className="btn"
+                type="button"
+                value="Annuler"
+                onClick={() => retour()}
+              />
+            </div>
+          </form>
+        ) : (
+          "coucou"
+        )}
       </div>
     </Provider>
   );
